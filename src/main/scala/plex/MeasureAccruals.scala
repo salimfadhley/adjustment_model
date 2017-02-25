6c1acdbd71a4d70216c0d20505eeeda19557ed6f
Salim Fadhley <salimfadhley@gmail.com>
2017-02-25 23:44:19 +0000

    Tweak the accrual logic

--- src/main/scala/plex/MeasureAccruals.scala
+++ src/main/scala/plex/MeasureAccruals.scala
@@ -3,20 +3,29 @@ package plex
 import adjustments.BalanceSheets
 import adjustments.BalanceSheets.BalanceSheets
 
+import scala.collection.immutable
+
 /**
   * Created by salim on 2/25/2017.
   */
 object MeasureAccruals {
-  val subledger_accrued_balance_sheets = List(BalanceSheets.PV, BalanceSheets.Reserves)
 
-  def calculateAccruals(s: String, measureType:MeasureType.Value, balanceSheet:Option[BalanceSheets]):List[String] = {
-    (s, measureType, balanceSheet) match {
-      case (m:String, MeasureType.PlexMeasure, b) if subledger_accrued_balance_sheets.contains(b)=>
-        List("TotalPnL", "TotalPnLExcludingReserves", "FinancePnL")
-      case (m:String, MeasureType.PlexMeasure, b) =>
-        List("TotalPnL", "TotalPnLExcludingReserves")
+  def calculateAccruals(measure: String, measureType:MeasureType.Value, balanceSheet:Option[BalanceSheets]):List[String] = {
+
+    measureType match {
+      case MeasureType.PlexMeasure => List("TotalPnL") ++ {
+        balanceSheet match {
+          case Some(x) if List(BalanceSheets.PV, BalanceSheets.Reserves).contains(x) => List("FinancePnL")
+          case _ => Nil
+        }
+      } ++ { measure match {
+        case "Brokerage" => Nil
+        case _ => List("TotalPnLExcludingBrokerage")
+      }}
       case _ => Nil
     }
+
+
   }
 
 }
--- src/test/scala/plex/MeasureAccrualsSpec.scala
+++ src/test/scala/plex/MeasureAccrualsSpec.scala
@@ -11,15 +11,15 @@ class MeasureAccrualsSpec extends FlatSpec with Matchers {
   "MeasureAccruals" should "know that all plex measures accrue to totalPnl" in {
     val accruals = MeasureAccruals.calculateAccruals("xxx", MeasureType.PlexMeasure, Some(BalanceSheets.PV))
     accruals should contain("TotalPnL")
-    accruals should contain("TotalPnLExcludingReserves")
+    accruals should contain("TotalPnLExcludingBrokerage")
     accruals should contain("FinancePnL")
   }
 
   it should "know that calculated measures do not generate additional totals" in {
-    val accruals = MeasureAccruals.calculateAccruals("xxx", MeasureType.Calculated, Some(BalanceSheets.Bonds))
+    val accruals = MeasureAccruals.calculateAccruals("Brokerage", MeasureType.PlexMeasure, Some(BalanceSheets.PV))
     accruals should contain("TotalPnL")
-    accruals should contain("TotalPnLExcludingReserves")
-    accruals should not contain("FinancePnL")
+    accruals should not contain("TotalPnLExcludingBrokerage")
+    accruals should contain("FinancePnL")
   }
 
   it should "know Calculated measures do not generate additional totals" in {
